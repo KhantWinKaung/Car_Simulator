e36aaa36a755953fdeb9e49d8aed297fc85d946d
Kaung Khant Win <kkw.kaungkhant@gmail.com>
2019-12-27 16:00:30 +0800

    TrafficLight.java update

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -5,13 +5,13 @@ public class TrafficLight {
     public TrafficLight() {
     }
 
-    // TrafficLight constructor
+    // Traffic Light constructor
     public TrafficLight(String color, double rateOfChange){
         this.color = color;
         this.rateOfChange = rateOfChange;
     }
 
-    // operate method
+    // operate method for traffic light
     public void operate(){
         if(rateOfChange < (Math.random() * ((1.0 - 10.0) + 1)) + 10.0)
             this.color = "Red";
